216b764d7f10c0b35954f9555d8ca11b44a74ece
Isaac71412079 <isaac.rivero@ucb.edu.bo>
2025-11-21 15:52:34 -0400

    Primer commit

---
+++ src/sumador.test.js
@@ -0,0 +1,10 @@
+/* eslint-disable quotes */
+import sumar from "./sumador.js";
+
+describe("Sumar", () => {
+  it("deberia sumar dos numeros", () => {
+    expect(sumar(3, 2)).toEqual(5);
+  });
+});
+
+
